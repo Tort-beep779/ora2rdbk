1bcf050b1a0c06a60088020c947d55e39a04bcc3
Anton Zuev <anton-zuev@red-soft.ru>
2023-03-31 17:54:28 +0300

    Make tests check only difference between an expected result and actual output.

--- ora2rdb/src/test/java/ru/redsoft/ora2rdb/ParserTest.java
+++ ora2rdb/src/test/java/ru/redsoft/ora2rdb/ParserTest.java
@@ -5,22 +5,37 @@ import org.junit.jupiter.api.Test;
 import java.io.*;
 import java.nio.charset.*;
 import java.nio.file.*;
-
+import com.github.difflib.DiffUtils;
+import com.github.difflib.UnifiedDiffUtils;
+import com.github.difflib.patch.Patch;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest {
-    private String readFile(String path, Charset encoding) throws IOException {
-        return Files.readString(Paths.get("src/test/resources/"+path), encoding);
+    private List readFile(String path) throws IOException {
+        List<String> str = new ArrayList<>();
+        BufferedReader reader = new BufferedReader
+                (new InputStreamReader(
+                        new FileInputStream("src/test/resources/"+path), StandardCharsets.UTF_8));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            str.add(line);
+        }
+        return str;
     }
 
     void test(String file) throws IOException {
-        String expected = readFile("output/" + file, StandardCharsets.UTF_8);
+        List<String> expected = readFile("output/" + file);
 
         RewritingListener rewritingListener =
                 Ora2rdb.convert(new FileInputStream("src/test/resources/input/" + file));
-        String actual = rewritingListener.rewriter.getText().replace("\r", "");
-        assertEquals(expected, actual);
+        List<String> actual = Arrays.asList(rewritingListener.rewriter.getText().replace("\r", "").split("\n"));
+
+        Patch diff = DiffUtils.diff(expected, actual);
+        List<String> unifiedDiff = UnifiedDiffUtils.generateUnifiedDiff("expected", "actual", expected, diff, 0);
+        String result = String.join("\n", unifiedDiff);
+        assertEquals("", result);
     }
 
     @Test

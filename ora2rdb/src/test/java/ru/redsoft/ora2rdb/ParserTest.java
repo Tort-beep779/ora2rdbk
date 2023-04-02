a8cf25ddfb6fe06be8925664d026bc2a100a4fc7
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2023-04-02 17:06:31 +0300

    Cosmetic

--- ora2rdb/src/test/java/ru/redsoft/ora2rdb/ParserTest.java
+++ ora2rdb/src/test/java/ru/redsoft/ora2rdb/ParserTest.java
@@ -2,40 +2,40 @@ package ru.redsoft.ora2rdb;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
-import java.nio.charset.*;
-import java.nio.file.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
 import com.github.difflib.DiffUtils;
 import com.github.difflib.UnifiedDiffUtils;
 import com.github.difflib.patch.Patch;
-import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserTest {
-    private List readFile(String path) throws IOException {
-        List<String> str = new ArrayList<>();
-        BufferedReader reader = new BufferedReader
-                (new InputStreamReader(
-                        new FileInputStream("src/test/resources/"+path), StandardCharsets.UTF_8));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            str.add(line);
-        }
-        return str;
+    private List<String> readFile(String path) throws IOException {
+        return Files.readAllLines(Path.of("src/test/resources/" + path), StandardCharsets.UTF_8);
     }
 
     void test(String file) throws IOException {
         List<String> expected = readFile("output/" + file);
 
-        RewritingListener rewritingListener =
-                Ora2rdb.convert(new FileInputStream("src/test/resources/input/" + file));
-        List<String> actual = Arrays.asList(rewritingListener.rewriter.getText().replace("\r", "").split("\n"));
+        try (FileInputStream fs = new FileInputStream("src/test/resources/input/" + file)) {
+            RewritingListener rewritingListener =
+                    Ora2rdb.convert(fs);
+            List<String> actual = Arrays.asList(rewritingListener.rewriter.getText()
+                    .replace("\r", "").split("\n"));
 
-        Patch diff = DiffUtils.diff(expected, actual);
-        List<String> unifiedDiff = UnifiedDiffUtils.generateUnifiedDiff("expected", "actual", expected, diff, 0);
-        String result = String.join("\n", unifiedDiff);
-        assertEquals("", result);
+            Patch<String> diff = DiffUtils.diff(expected, actual);
+            List<String> unifiedDiff = UnifiedDiffUtils.generateUnifiedDiff("expected",
+                    "actual", expected, diff, 0);
+            String result = String.join("\n", unifiedDiff);
+            assertEquals("", result);
+        }
     }
 
     @Test

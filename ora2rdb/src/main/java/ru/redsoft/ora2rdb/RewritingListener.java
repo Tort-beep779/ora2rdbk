3ec2ec056e7a0102bc7e76694665590861b0f7cb
Denis Vicente <100801771+DenArto@users.noreply.github.com>
2023-04-27 09:16:05 +0300

    Removing duplicate code. removing the substr function conversion algorithm

--- ora2rdb/src/main/java/ru/redsoft/ora2rdb/RewritingListener.java
+++ ora2rdb/src/main/java/ru/redsoft/ora2rdb/RewritingListener.java
@@ -424,22 +424,7 @@ public class RewritingListener extends PlSqlParserBaseListener {
                                 if (function_argument_ctx.argument().size() == 2)
                                     insertAfter(function_argument_ctx.argument(1), ", ''");
                         }
-                    } else if (Ora2rdb.getRealName(getRuleText(regular_id_ctx)).equals("SUBSTR")) {
-                        if (ctx.function_argument() != null) {
-                            replace(regular_id_ctx, "SUBSTRING");
-                            Function_argumentContext function_argument_ctx = ctx.function_argument();
-
-                            if (function_argument_ctx.argument().size() == 2) {
-                                replace(function_argument_ctx, "(" + getRewriterText(function_argument_ctx.argument(0)) +
-                                        " FROM " + getRewriterText(function_argument_ctx.argument(1)) + ")");
-                            } else if (function_argument_ctx.argument().size() == 3) {
-                                replace(function_argument_ctx, "(" + getRewriterText(function_argument_ctx.argument(0)) +
-                                        " FROM " + getRewriterText(function_argument_ctx.argument(1)) +
-                                        " FOR " + getRewriterText(function_argument_ctx.argument(2)) + ")");
-                            }
-                        }
                     }
-
                     replace(regular_id_ctx.non_reserved_keywords_pre12c().LENGTH(), "CHAR_LENGTH");
                 }
             }

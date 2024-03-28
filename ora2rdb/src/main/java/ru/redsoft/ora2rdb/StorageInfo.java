64ab190aa1e187d4c90583ded825eb1c2872bf6c
Denis Vicente <denis.visente@red-soft.ru>
2024-03-28 09:18:45 +0300

    Added StorageInfo class this class stores information received after the scan

---
+++ ora2rdb/src/main/java/ru/redsoft/ora2rdb/StorageInfo.java
@@ -0,0 +1,14 @@
+package ru.redsoft.ora2rdb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StorageInfo {
+    public static ArrayList<String> package_constant_names = new ArrayList<>();
+
+
+
+    public static void clearInfo(){
+        package_constant_names.clear();
+    }
+}

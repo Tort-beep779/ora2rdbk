5da20f845bd34b27384612a73bda56150859b2f9
Denis Vicente <denis.visente@red-soft.ru>
2024-11-08 11:14:51 +0300

    Reorganizing the testing system (RS-190967)
    One test is called with a set of parameters. The test result is displayed for each parameter.
    The set of parameters is selected from the directory structure located in the resources/scripts section.
    Each parameter is a test that includes one Oracle sql input file and one file with the expected result.
    These files must be in the same directory.
    You can specify a directory or set of directories to run only those tests that are in these directories:
    mvn test -DtestSuite=object/function
    mvn test -DtestSuite="object/function, object/procedure"
    
    File Naming Rules:
    The input file must have the ending ".sql" (input_file_name.sql).
    The file with the expected result must have the same name, but must have the ending "_out.sql" (input_file_name_out.sql).

--- ora2rdb/src/test/java/ru/redsoft/ora2rdb/ParserTest.java
+++ ora2rdb/src/test/java/ru/redsoft/ora2rdb/ParserTest.java
@@ -1,7 +1,5 @@
 package ru.redsoft.ora2rdb;
 
-import org.junit.jupiter.api.Test;
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -10,45 +8,71 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.github.difflib.DiffUtils;
 import com.github.difflib.UnifiedDiffUtils;
 import com.github.difflib.patch.Patch;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserTest {
-    private List<String> readFile(String path) throws IOException {
-        return Files.readAllLines(Path.of("src/test/resources/" + path), StandardCharsets.UTF_8);
+
+    static String startDirPath = "src/test/resources/scripts/";
+    static ArrayList<String> argsArray = new ArrayList<String>();
+    @BeforeAll
+    static void getArguments() {
+        Arrays.stream(System.getProperty("testSuite", "").split(","))
+                .forEach(arg -> {
+                    arg = arg.replace(" ", "");
+                    if (!arg.endsWith("/"))
+                        arg = arg.concat("/");
+                    argsArray.add(arg);
+                });
     }
 
-    void test(String file) throws IOException {
-        final String outFile = System.getProperty("java.io.tmpdir") + "/" + file;
+    private List<String> readFile(String path) throws IOException {
+        return Files.readAllLines(Path.of(startDirPath + path), StandardCharsets.UTF_8);
+    }
+
+    void test(String inputFile) throws IOException {
+        final Path outFile =  Paths.get(System.getProperty("java.io.tmpdir"),"out.sql");
+        String expectedFile = inputFile.replace(".sql", "_out.sql");
         List<String> actual;
-        try (FileInputStream fs = new FileInputStream("src/test/resources/input/" + file);
-             FileWriter fileWriter = new FileWriter(outFile)) {
+        try (FileInputStream fs = new FileInputStream(startDirPath + inputFile);
+             FileWriter fileWriter = new FileWriter(outFile.toAbsolutePath().toString())) {
             RewritingListener rewritingListener =
                     Ora2rdb.convert(fs);
             actual = Arrays.asList(rewritingListener.rewriter.getText()
                     .replace("\r", "").split("\n"));
             for (String line : actual) {
-                fileWriter.write(line + System.lineSeparator());
+                if(!line.equals(actual.get(actual.size()-1)))
+                    fileWriter.write(line + System.lineSeparator());
+                else
+                    fileWriter.write(line);
             }
         }
 
-        List<String> expected = readFile("output/" + file);
+        List<String> expected = readFile(expectedFile);
         Patch<String> diff = DiffUtils.diff(expected, actual);
-        List<String> unifiedDiff = UnifiedDiffUtils.generateUnifiedDiff("expected",
+                List<String> unifiedDiff = UnifiedDiffUtils.generateUnifiedDiff("expected",
                 "actual", expected, diff, 0);
         String result = String.join("\n", unifiedDiff);
         if (!result.isEmpty()) {
             ProcessBuilder processBuilder = new ProcessBuilder("python3",
                     "src/test/resources/main.py",
                     "-n",
-                    "src/test/resources/output/" + file,
-                    outFile);
+                    startDirPath + expectedFile,
+                    outFile.toAbsolutePath().toString());
             processBuilder.redirectErrorStream(true);
 
             Process process;
@@ -65,101 +89,40 @@ class ParserTest {
                 }
 
                 process.waitFor();
-                assertEquals("", difference.toString());
+                String infoStr = "+++ actual\n" +
+                                 "--- expected\n" +
+                                 "+ \n\n";
+                assertEquals(infoStr, difference.toString());
             } catch (IOException | InterruptedException e) {
                 throw new IOException(e);
             }
         }
     }
 
-    @Test
-    void createTableTest() throws IOException {
-        test("create_table.sql");
+    static Stream<String> argsProviderFactory() throws IOException {
+        ArrayList<String> fileNameArray = new ArrayList<>();
+        for(String arg : argsArray) {
+            try {
+                Files.walk(Paths.get(startDirPath + arg), Integer.MAX_VALUE)
+                        .filter(e -> !Files.isDirectory(e) && !e.toString().contains("_out.sql"))
+                        .collect(Collectors.toCollection(LinkedList::new))
+                        .descendingIterator()
+                        .forEachRemaining(path -> {
+                            fileNameArray.add(path.toString().replace(startDirPath, ""));
+                        });
+            }catch (IOException e){
+                throw new IOException("directory not found: " + arg);
+            }
+        }
+        return fileNameArray.stream();
     }
 
-    @Test
-    void createTableWithStorageTest() throws IOException {
-        test("create_table_with_storage.sql");
-    }
-
-    @Test
-    void fullTest() throws IOException {
-        test("full.sql");
-    }
-
-    @Test
-    void functionsTest() throws IOException {
-        test("functions.sql");
-    }
-
-    @Test
-    void indexesTest() throws IOException {
-        test("indexes.sql");
-    }
-
-    @Test
-    void proceduresTest() throws IOException {
-        test("procedures.sql");
-    }
-
-    @Test
-    void sequencesTest() throws IOException {
-        test("sequences.sql");
-    }
-
-    @Test
-    void triggersTest() throws IOException {
-        test("triggers.sql");
-    }
-
-    @Test
-    void viewsTest() throws IOException {
-        test("views.sql");
-    }
-
-    @Test
-    void constraintsTest() throws IOException {
-        test("constraints.sql");
-    }
-
-    @Test
-    void hrOraTest() throws IOException {
-        test("hr_ora.sql");
-    }
-
-    @Test
-    void rowtypeTest() throws IOException {
-        test("rowtype.sql");
-    }
-
-    @Test
-    void package_constantsTest() throws IOException {
-        test("package_constants.sql");
-    }
-
-    @Test
-    void loopsTest() throws IOException {
-        test("loops.sql");
-    }
-
-    @Test
-    void associative_arraysTest() throws IOException {
-        test("associative_arrays.sql");
-    }
-
-    @Test
-    void blocks_separationTest() throws IOException {
-        test("blocks_separation.sql");
-    }
-
-    @Test
-    void package_separationTest() throws IOException {
-        test("package_separation.sql");
-    }
-    @Test
-    void type_declarationTest() throws IOException {
-        test("type_declaration.sql");
+    @ParameterizedTest(name = "{arguments}")
+    @MethodSource("argsProviderFactory")
+    void testAllScripts(String argument) throws IOException {
+        test(argument);
     }
 
 
 }
+

6df423323d7096e54dfdf9151717d0a5b790f688
AlexanderBorisov <aleksandr.borisov@red-soft.ru>
2024-12-12 17:41:15 +0300

    Fix test for create DETERMINISTIC function RS-206242

--- ora2rdb/src/test/resources/scripts/object/functions/create_function/DETERMENISTIC_expected.sql
+++ ora2rdb/src/test/resources/scripts/object/functions/create_function/DETERMENISTIC_expected.sql
@@ -2,8 +2,9 @@ CREATE FUNCTION betwnstr (
     string_in      VARCHAR,
     start_in       INTEGER,
     end_in         INTEGER
-    )
+)
     RETURNS VARCHAR
+    DETERMINISTIC
     SQL SECURITY DEFINER
 AS
 BEGIN
